f177f677a0476a013fb87bb119b42116cfb71668
joaofelix27 <joaofelix27@gmail.com>
2022-04-01 21:27:56 -0300

    Versão JS - Fechar Pedido Incompleto

--- scripts.js
+++ scripts.js
@@ -1,61 +1,113 @@
-function escolherprato (nome) {
-    let clicado = nome.querySelector(".ativo");
-    let icon1= nome.querySelector(".icone") 
-    const pratos = document.querySelectorAll(".pratos .container_comida");
-    pratos.forEach(prato => {
-        if(prato.classList.contains("ativo"))
-        {
-        prato.classList.remove("ativo");
-        prato.querySelector(".icone").innerHTML = "";
+let valorAtual=0
+
+function escolherprato(nome) {
+  let clicado = document.querySelector(".ativo");
+  let icone = nome.querySelector(".icone");
+  const pratos = document.querySelectorAll(".pratos .container_comida");
+  if(clicado==nome) {
+      clicado.classList.remove("ativo")
+      icone.innerHTML = "";
+      valorAtual=valorAtual-1
+  } else {
+                pratos.forEach((prato) => {
+                    if (prato.classList.contains("ativo")) {
+                        
+                    prato.classList.remove("ativo");
+                    prato.querySelector(".icone").innerHTML = "";
+                    }
+                });
+                    nome.classList.add("ativo");
+                    icone.innerHTML = '<ion-icon  name="checkmark-circle"></ion-icon>';
+                    prato = "Conteúdo";
+                    valorAtual=valorAtual+1
         }
-        
-    });
-    if (clicado==nome){
+        atualizarValor()
+  /*
+  let itemescondido = document.querySelector(".relativo");
+  if (itemescondido==nome){
+      itemescondido.classList.remove("relativo")
+  }
+else{   if (itemescondido !== null){ 
+       itemescondido.classList.remove("relativo")
+       }
+      nome.classList.add("relativo")
+  }
+  */
+}
+function escolherbebida(nome) {
+    let clicado = document.querySelector(".ativo");
+    let icone = nome.querySelector(".icone");
+    const bebidas = document.querySelectorAll(".bebidas .container_comida");
+    if(clicado==nome) {
         clicado.classList.remove("ativo")
-        icon1.style.display="none"
-    }
- else{   if (clicado !== null){ 
-         clicado.classList.remove("ativo")
-         icon1.style.display="none"
-         }
-        nome.classList.add("ativo")
-        icon1.style.display="initial"
-    }
+        icone.innerHTML = "";
+        valorAtual=valorAtual-1
+    } else {
+                  bebidas.forEach((prato) => {
+                      if (prato.classList.contains("ativo")) {
+                          
+                      prato.classList.remove("ativo");
+                      prato.querySelector(".icone").innerHTML = "";
+                      }
+                  });
+                      nome.classList.add("ativo");
+                      icone.innerHTML = '<ion-icon  name="checkmark-circle"></ion-icon>';
+                      prato = "Conteúdo";
+                      valorAtual=valorAtual+1
+          }
+          atualizarValor()
     /*
     let itemescondido = document.querySelector(".relativo");
     if (itemescondido==nome){
         itemescondido.classList.remove("relativo")
     }
- else{   if (itemescondido !== null){ 
+  else{   if (itemescondido !== null){ 
          itemescondido.classList.remove("relativo")
          }
         nome.classList.add("relativo")
     }
     */
-}
-function escolherbebida (nome) {
-    let clicado = document.querySelector(".escolhido_bebida");
-    if (clicado==nome){
-        clicado.classList.remove("escolhido_bebida")
+  }
+  function escolhersobremesa(nome) {
+    let clicado = document.querySelector(".ativo");
+    let icone = nome.querySelector(".icone");
+    const sobremesas = document.querySelectorAll(".sobremesas .container_comida");
+    if(clicado==nome) {
+        clicado.classList.remove("ativo")
+        icone.innerHTML = "";
+        valorAtual=valorAtual-1
+    } else {
+                  sobremesas.forEach((prato) => {
+                      if (prato.classList.contains("ativo")) {
+                          
+                      prato.classList.remove("ativo");
+                      prato.querySelector(".icone").innerHTML = "";
+                      }
+                  });
+                      nome.classList.add("ativo");
+                      icone.innerHTML = '<ion-icon  name="checkmark-circle"></ion-icon>';
+                      prato = "Conteúdo";
+                      valorAtual=valorAtual+1
+          }
+          atualizarValor()
+    /*
+    let itemescondido = document.querySelector(".relativo");
+    if (itemescondido==nome){
+        itemescondido.classList.remove("relativo")
     }
-  
- else{   if (clicado !== null){ 
-         clicado.classList.remove("escolhido_bebida")
+  else{   if (itemescondido !== null){ 
+         itemescondido.classList.remove("relativo")
          }
-        nome.classList.add("escolhido_bebida")
+        nome.classList.add("relativo")
     }
-}
-function escolhersobremesa (nome) {
-    let clicado = document.querySelector(".escolhido_sobremesa");
-    if (clicado==nome){
-        clicado.classList.remove("escolhido_sobremesa")
-    }
-  
- else{   if (clicado !== null){ 
-         clicado.classList.remove("escolhido_sobremesa")
-         }
-        nome.classList.add("escolhido_sobremesa")
-   
-    }
-} 
+    */
+  }
 /*      document.getElementById("carlos").style.display = "initial"; */
+function atualizarValor() {
+    if (valorAtual ==3) {
+        let stewart= document.querySelector(".inferior_cinza");
+        let stewart1= document.querySelector(".inferior_verde");
+        stewart.style.display="none"
+        stewart1.style.display="initial"
+    } 
+  }
