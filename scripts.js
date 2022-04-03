1a7a899dac78570aef43acf2176e1fe5f55559b4
joaofelix27 <joaofelix27@gmail.com>
2022-04-03 18:24:13 -0300

    Versão Requisitos OK

--- scripts.js
+++ scripts.js
@@ -1,25 +1,40 @@
 let valorAtual=0
+let prato=""
+let bebida=""
+let sobremesa=""
+let precofinal=0
 
 function escolherprato(nome) {
-  let clicado = document.querySelector(".ativo");
+  let clicado = document.querySelector(".ativo1");
   let icone = nome.querySelector(".icone");
   const pratos = document.querySelectorAll(".pratos .container_comida");
+  let precoparcial=0
   if(clicado==nome) {
-      clicado.classList.remove("ativo")
-      icone.innerHTML = "";
-      valorAtual=valorAtual-1
+        precoparcial=document.querySelector(".ativo1").querySelector(".preço")
+        precoparcial=Number(precoparcial.innerHTML.replace(",",".").replace("R$"," "))
+        precofinal= precofinal - precoparcial
+        clicado.classList.remove("ativo1")
+        icone.innerHTML = "";
+        valorAtual=valorAtual-1
   } else {
                 pratos.forEach((prato) => {
-                    if (prato.classList.contains("ativo")) {
-                        
-                    prato.classList.remove("ativo");
-                    prato.querySelector(".icone").innerHTML = "";
+                    if (prato.classList.contains("ativo1")) {
+                    precoparcial=document.querySelector(".ativo1").querySelector(".preço")
+                     precoparcial=Number(precoparcial.innerHTML.replace(",",".").replace("R$"," "))
+                     precofinal= precofinal - precoparcial
+                    prato.classList.remove("ativo1");
+                    valorAtual=valorAtual-1
+                     prato.querySelector(".icone").innerHTML = ""
                     }
+                    
                 });
-                    nome.classList.add("ativo");
+                    nome.classList.add("ativo1");
                     icone.innerHTML = '<ion-icon  name="checkmark-circle"></ion-icon>';
-                    prato = "Conteúdo";
                     valorAtual=valorAtual+1
+                    prato = nome.querySelector(".nome_comida").innerText;
+                    precoparcial=document.querySelector(".ativo1").querySelector(".preço")
+                    precoparcial=Number(precoparcial.innerHTML.replace(",",".").replace("R$"," "))
+                    precofinal+= Number(precoparcial)
         }
         atualizarValor()
   /*
@@ -35,25 +50,37 @@ else{   if (itemescondido !== null){
   */
 }
 function escolherbebida(nome) {
-    let clicado = document.querySelector(".ativo");
+    let clicado = document.querySelector(".ativo2");
     let icone = nome.querySelector(".icone");
     const bebidas = document.querySelectorAll(".bebidas .container_comida");
+    let precoparcial=0
     if(clicado==nome) {
-        clicado.classList.remove("ativo")
+        precoparcial=document.querySelector(".ativo2").querySelector(".preço")
+        precoparcial=Number(precoparcial.innerHTML.replace(",",".").replace("R$"," "))
+        precofinal= precofinal - precoparcial
+        clicado.classList.remove("ativo2")
         icone.innerHTML = "";
         valorAtual=valorAtual-1
     } else {
                   bebidas.forEach((prato) => {
-                      if (prato.classList.contains("ativo")) {
-                          
-                      prato.classList.remove("ativo");
-                      prato.querySelector(".icone").innerHTML = "";
+                      if (prato.classList.contains("ativo2")) {
+                        precoparcial=document.querySelector(".ativo2").querySelector(".preço")
+                     precoparcial=Number(precoparcial.innerHTML.replace(",",".").replace("R$"," "))
+                     precofinal= precofinal - precoparcial
+                    prato.classList.remove("ativo2");
+                    valorAtual=valorAtual-1
+                     prato.querySelector(".icone").innerHTML = ""
                       }
+                      
                   });
-                      nome.classList.add("ativo");
-                      icone.innerHTML = '<ion-icon  name="checkmark-circle"></ion-icon>';
-                      prato = "Conteúdo";
-                      valorAtual=valorAtual+1
+                  nome.classList.add("ativo2");
+                  icone.innerHTML = '<ion-icon  name="checkmark-circle"></ion-icon>';
+                  valorAtual=valorAtual+1
+                  bebida = nome.querySelector(".nome_comida").innerText;
+                  precoparcial=document.querySelector(".ativo2").querySelector(".preço")
+                  precoparcial=Number(precoparcial.innerHTML.replace(",",".").replace("R$"," "))
+                  precofinal+= Number(precoparcial)
+                      
           }
           atualizarValor()
     /*
@@ -69,25 +96,34 @@ function escolherbebida(nome) {
     */
   }
   function escolhersobremesa(nome) {
-    let clicado = document.querySelector(".ativo");
+    let clicado = document.querySelector(".ativo3");
     let icone = nome.querySelector(".icone");
     const sobremesas = document.querySelectorAll(".sobremesas .container_comida");
     if(clicado==nome) {
-        clicado.classList.remove("ativo")
+        precoparcial=document.querySelector(".ativo3").querySelector(".preço")
+        precoparcial=Number(precoparcial.innerHTML.replace(",",".").replace("R$"," "))
+        precofinal= precofinal - precoparcial
+        clicado.classList.remove("ativo3")
         icone.innerHTML = "";
         valorAtual=valorAtual-1
     } else {
                   sobremesas.forEach((prato) => {
-                      if (prato.classList.contains("ativo")) {
-                          
-                      prato.classList.remove("ativo");
-                      prato.querySelector(".icone").innerHTML = "";
+                      if (prato.classList.contains("ativo3")) {
+                        precoparcial=document.querySelector(".ativo3").querySelector(".preço")
+                        precoparcial=Number(precoparcial.innerHTML.replace(",",".").replace("R$"," "))
+                        precofinal= precofinal - precoparcial
+                       prato.classList.remove("ativo3");
+                       valorAtual=valorAtual-1
+                        prato.querySelector(".icone").innerHTML = ""
                       }
                   });
-                      nome.classList.add("ativo");
-                      icone.innerHTML = '<ion-icon  name="checkmark-circle"></ion-icon>';
-                      prato = "Conteúdo";
-                      valorAtual=valorAtual+1
+                  nome.classList.add("ativo3");
+                  icone.innerHTML = '<ion-icon  name="checkmark-circle"></ion-icon>';
+                  valorAtual=valorAtual+1
+                  sobremesa = nome.querySelector(".nome_comida").innerText;
+                  precoparcial=document.querySelector(".ativo3").querySelector(".preço")
+                  precoparcial=Number(precoparcial.innerHTML.replace(",",".").replace("R$"," "))
+                  precofinal+= Number(precoparcial)
           }
           atualizarValor()
     /*
@@ -104,10 +140,23 @@ function escolherbebida(nome) {
   }
 /*      document.getElementById("carlos").style.display = "initial"; */
 function atualizarValor() {
-    if (valorAtual ==3) {
-        let stewart= document.querySelector(".inferior_cinza");
+    let stewart= document.querySelector(".inferior_cinza");
         let stewart1= document.querySelector(".inferior_verde");
+    if (valorAtual ==3) {
         stewart.style.display="none"
-        stewart1.style.display="initial"
-    } 
+        stewart1.style.display="flex"
+    } else{
+        stewart.style.display="flex"
+        stewart1.style.display="none"
+    }
+  }
+  function fecharpedido( ) {
+      let  mensagem
+      let nomex= prompt ("Qual é o seu nome?")
+      let endereço = prompt ("Qual é o seu endereço?")
+      precofinal= (Math.round(precofinal * 100) / 100).toFixed(2);
+      mensagem =   `Olá, gostaria de fazer o pedido: \n - Prato: ${prato} \n - Bebida: ${bebida} \n - Sobremesa: ${sobremesa} \n Total: R$ ${precofinal} \n \n Nome: ${nomex} \n Endereço: ${endereço}`
+      let mensagemenconded = encodeURIComponent(mensagem);
+      alert (mensagem)
+      window.open (`https://wa.me/+5583991005321?text=${mensagemenconded}`,'_blank');
   }
